26ebee84f43a43153ae12116f580adfd7b57505f
SHAH7766 <shahussainbadshah786@gmail.com>
2026-04-24 19:30:39 +0500

    Server file updated

--- Server/Server.js
+++ Server/Server.js
@@ -7,7 +7,11 @@ import cors from 'cors'
 dotenv.config()
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(app.use(cors({
+  origin: 'https://pro-connect-v6i2.vercel.app/', // Replace with your actual Vercel URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));))
 app.use("/api",router)
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running at port ${process.env.PORT}`.bgBrightBlue)
